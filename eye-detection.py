1b44908ed57dcbafdfc4317e4ee2cbc51f4cf739
Daboss-1 <139522291+Daboss-1@users.noreply.github.com>
2024-07-18 11:12:07 -0400

    Detects foreface and background face - color coded

--- eye-detection.py
+++ eye-detection.py
@@ -1,41 +1,54 @@
 import cv2
+import numpy as np
 
 eye_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml')
-face_cascade = cv2.CascadeClassifier('haarcascades\haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
 
-one_eye = cv2.imread('testing/one-eye-open.jpg',0)
-two_eyes = cv2.imread('testing/2-eyes.jpeg',0)
-no_eyes = cv2.imread('testing/closed-eyes.jpeg',0)
+one_eye = cv2.imread('testing/one-eye-open.jpg', 0)
+two_eyes = cv2.imread('testing/2-eyes.jpeg', 0)
+no_eyes = cv2.imread('testing/closed-eyes.jpeg', 0)
 
 def detect_eyes(img):
     face_img = img.copy()
-    eyes = eye_cascade.detectMultiScale(face_img,scaleFactor=1.35,minNeighbors=5) 
-    
-    return len(eyes)
-
-def detect_eyes_image(img):
-    face_img = img.copy()
-    eyes = eye_cascade.detectMultiScale(face_img,scaleFactor=1.35,minNeighbors=5) 
-    
-    for (x,y,w,h) in eyes: 
-        cv2.rectangle(face_img, (x,y), (x+w,y+h), (255,255,255), 10) 
-    return face_img
+    eyes = eye_cascade.detectMultiScale(face_img, scaleFactor=1.2, minNeighbors=7)
+    return eyes
 
 def detect_face(img):
     face_img = img.copy()
-    faces = face_cascade.detectMultiScale(face_img,scaleFactor=1.5,minNeighbors=5) 
-    
-    for (x,y,w,h) in faces: 
-        cv2.rectangle(face_img, (x,y), (x+w,y+h), (255,255,255), 5) 
-    return face_img
+    faces = face_cascade.detectMultiScale(face_img, scaleFactor=1.2, minNeighbors=5)
+    return faces
 
-one_eye_result = detect_eyes(one_eye)
-two_eye_result = detect_eyes(two_eyes)
-no_eye_result = detect_eyes(no_eyes)
+def draw_rect(img, cascade, color):
+    result = img.copy()
+    for (x, y, w, h) in cascade:
+        cv2.rectangle(result, (x, y), (x + w, y + h), color, 5)
+    return result
 
-cv2.imwrite('testing/no-eye-results.jpeg', detect_eyes_image(no_eyes))
-cv2.imwrite('testing/one-eye-results.jpeg', detect_eyes_image(one_eye))
-cv2.imwrite('testing/two-eye-results.jpeg', detect_eyes_image(two_eyes))
+def get_face_foreground(cascade):
+    if len(cascade) == 1 or len(cascade) == 0:
+        return cascade
+    else:
+        max_area = 0
+        final_face = None
+        for (x, y, w, h) in cascade:
+            area = w * h
+            if area > max_area:
+                max_area = area
+                final_face = (x, y, w, h)
+        return [final_face]
+
+def get_background_faces(faces):
+    foreground_face = get_face_foreground(faces)
+    background_faces = [face for face in faces if not any(np.array_equal(face, ff) for ff in foreground_face)]
+    return background_faces
+
+one_eye_result = len(detect_eyes(one_eye))
+two_eye_result = len(detect_eyes(two_eyes))
+no_eye_result = len(detect_eyes(no_eyes))
+
+cv2.imwrite('testing/no-eye-results.jpeg', draw_rect(no_eyes, detect_eyes(no_eyes), (0,255,0)))
+cv2.imwrite('testing/one-eye-results.jpeg', draw_rect(one_eye, detect_eyes(one_eye), (0,255,0)))
+cv2.imwrite('testing/two-eye-results.jpeg', draw_rect(two_eyes, detect_eyes(two_eyes), (0,255,0)))
 
 print(f"{no_eye_result} eye(s) were/was detected for a picture with zero eyes (closed eyes).")
 print(f"{one_eye_result} eye(s) were/was detected for a picture with one eye.")
@@ -44,9 +57,13 @@ print("Initializing camera...")
 cap = cv2.VideoCapture(0)
 print("Camera initialized!")
 while True:
-    ret,frame = cap.read()
-    frame = (detect_eyes_image(frame))
-    cv2.imshow('Eye Detection',frame)
+    ret, frame = cap.read()
+    background_faces = get_background_faces(detect_face(frame))
+    face_from_frame = get_face_foreground(detect_face(frame))
+    eyes_from_frame = detect_eyes(frame)
+    frame = draw_rect(draw_rect(draw_rect(frame, background_faces, (255, 0, 0)), face_from_frame, (255, 255, 255)), eyes_from_frame, (0, 255, 0))
+
+    cv2.imshow('Eye Detection', frame)
     code = cv2.waitKey(10)
     if code == ord('q'):
         break
